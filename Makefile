87d434495691679a24c306d550efbb824332fc3d
Guillaume Bour <guillaume@bour.cc>
2019-12-07 10:50:22 +0100

    [feat] implement acme v2 - partial
    refactors backend modules (api, jws, ssl)

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 SHELL=bash
 
+all:
+	./rebar3 upgrade
+	./rebar3 compile
+
 dialize:
 	if [[ "$$TRAVIS_OTP_RELEASE" > "18" ]] ; then\
 		./rebar3 dialyzer;\
--- src/letsencrypt_jws.erl
+++ src/letsencrypt_jws.erl
@@ -16,6 +16,9 @@
 -author("Guillaume Bour <guillaume@bour.cc>").
 
 -export([init/1, encode/3, thumbprint/2]).
+% v2
+-export([thumbprint2/2]).
+
 
 -spec init(letsencrypt:ssl_privatekey()) -> letsencrypt:jws().
 init(#{b64 := {N,E}}) ->
@@ -30,17 +33,21 @@ init(#{b64 := {N,E}}) ->
     }.
 
 
--spec encode(letsencrypt:ssl_privatekey(), letsencrypt:jws(), map()) -> binary().
+-spec encode(letsencrypt:ssl_privatekey(), letsencrypt:jws(), map()|empty) -> binary().
 encode(#{raw := RSAKey}, Jws, Content) ->
     %io:format("~p~n~p~n", [Jws, Content]),
     Protected = letsencrypt_utils:b64encode(jiffy:encode(Jws)),
-    Payload   = letsencrypt_utils:b64encode(jiffy:encode(Content)),
+	Payload = case Content of
+		% for POST-as-GET queries, payload is just an empty string
+		empty -> <<"">>;
+		_     -> letsencrypt_utils:b64encode(jiffy:encode(Content))
+	end,
 
     Sign  = crypto:sign(rsa, sha256, <<Protected/binary, $., Payload/binary>>, RSAKey),
     Sign2 = letsencrypt_utils:b64encode(Sign),
 
     jiffy:encode({[
-        {header, {[]}},
+        %{header, {[]}},
         {protected, Protected},
         {payload  , Payload},
         {signature, Sign2}
@@ -59,3 +66,11 @@ thumbprint(#{b64 := {N,E}}, Token) ->
 
     <<Token/binary, $., (letsencrypt_utils:b64encode(crypto:hash(sha256, Thumbprint)))/binary>>.
 
+thumbprint2(#{<<"e">> := E, <<"n">> := N, <<"kty">> := Kty}, Token) ->
+	Thumbprint = jiffy:encode({[
+		{e, E},
+		{kty, Kty},
+		{n, N}
+	]}, [force_utf8]),
+
+    <<Token/binary, $., (letsencrypt_utils:b64encode(crypto:hash(sha256, Thumbprint)))/binary>>.
